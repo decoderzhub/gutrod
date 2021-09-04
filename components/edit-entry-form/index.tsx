c2649ff5a8030a7a5350e5d268abde2d6b938dfc
decoderzhub <darin.j.manley@gmail.com>
2021-09-03 21:38:28 -0700

    update entry form

--- components/edit-entry-form/index.tsx
+++ components/edit-entry-form/index.tsx
@@ -5,19 +5,39 @@ import Button from '../button'
 
 export default function EntryForm() {
   const [_title, setTitle] = useState('')
+  const [_firstname, setFirstName] = useState('')
+  const [_lastname, setLastName] = useState('')
+  const [_email, setEmail] = useState('')
+  const [_phone, setPhone] = useState('')
+  const [_amcontact, setAmcontact] = useState('')
   const [_content, setContent] = useState('')
   const [submitting, setSubmitting] = useState(false)
   const router = useRouter()
-  const { id, title, content } = router.query
+  const { id, firstname, lastname, email, phone, amcontact, title, content } = router.query
 
   useEffect(() => {
+    if (typeof firstname === 'string') {
+      setFirstName(firstname)
+    }
+    if (typeof lastname === 'string') {
+      setLastName(lastname)
+    }
+    if (typeof email === 'string') {
+      setEmail(email)
+    }
+    if (typeof phone === 'string') {
+      setPhone(phone)
+    }
+    if (typeof amcontact === 'string') {
+      setAmcontact(amcontact)
+    }
     if (typeof title === 'string') {
       setTitle(title)
     }
     if (typeof content === 'string') {
       setContent(content)
     }
-  }, [title, content])
+  }, [firstname, lastname, email, phone, amcontact, title, content])
 
   async function submitHandler(e) {
     e.preventDefault()
@@ -32,6 +52,11 @@ export default function EntryForm() {
           id,
           title: _title,
           content: _content,
+          firstname: _firstname,
+          lastname: _lastname,
+          email: _email,
+          phone: _phone,
+          amcontact: _amcontact
         }),
       })
       const json = await res.json()
@@ -56,6 +81,58 @@ export default function EntryForm() {
           name="title"
           value={_title}
           onChange={(e) => setTitle(e.target.value)}
+        />
+      </div>
+      <div className="my-4">
+        <label htmlFor="firstname">
+          <h3 className="font-bold">First Name</h3>
+        </label>
+        <input
+          id="firstname"
+          className="shadow border rounded w-full"
+          type="text"
+          name="firstname"
+          value={_firstname}
+          onChange={(e) => setFirstName(e.target.value)}
+        />
+      </div>
+      <div className="my-4">
+        <label htmlFor="lastname">
+          <h3 className="font-bold">Last Name</h3>
+        </label>
+        <input
+          id="lastname"
+          className="shadow border rounded w-full"
+          type="text"
+          name="lastname"
+          value={_lastname}
+          onChange={(e) => setLastName(e.target.value)}
+        />
+      </div>
+      <div className="my-4">
+        <label htmlFor="email">
+          <h3 className="font-bold">Email</h3>
+        </label>
+        <input
+          id="email"
+          className="shadow border rounded w-full"
+          type="email"
+          name="firstname"
+          value={_email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <div className="my-4">
+        <label htmlFor="amcontact">
+          <h3 className="font-bold">American Contact Name</h3>
+        </label>
+        <input
+          id="amcontact"
+          className="shadow border rounded w-full"
+          type="text"
+          name="amcontact"
+          value={_amcontact}
+          onChange={(e) => setAmcontact(e.target.value)}
         />
       </div>
       <div className="my-4">
