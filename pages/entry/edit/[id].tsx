8e4be68288e09853ab8f21316ccac4765a14b162
decoderzhub <darin.j.manley@gmail.com>
2021-09-06 05:35:02 -0700

    revert edit page

--- pages/entry/edit/[id].tsx
+++ pages/entry/edit/[id].tsx
@@ -5,7 +5,7 @@ import EditEntryForm from '@/components/edit-entry-form'
 export default function EditEntryPage() {
   return (
     <>
-      <Nav title="Change" />
+      <Nav title="Edit" />
       <Container>
         <EditEntryForm />
       </Container>
