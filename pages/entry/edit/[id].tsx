d1faa3fc98b9d9de7d2c5e6dde80776c52deccf6
decoderzhub <darin.j.manley@gmail.com>
2021-09-06 05:39:58 -0700

    test

--- pages/entry/edit/[id].tsx
+++ pages/entry/edit/[id].tsx
@@ -5,7 +5,7 @@ import EditEntryForm from '@/components/edit-entry-form'
 export default function EditEntryPage() {
   return (
     <>
-      <Nav title="Edit" />
+      <Nav title="test" />
       <Container>
         <EditEntryForm />
       </Container>
